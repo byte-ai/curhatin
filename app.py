a7319223e9fb5f69cd853a05e767fee5bbfa63d3
afrizal0 <wikimedicx4504@gmail.com>
2023-02-11 14:33:09 +0700

    create cta section

--- app.py
+++ app.py
@@ -2,16 +2,17 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def home():
 	return render_template("index.html")	
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
-	sentence = request.form["sentence"]		
-	
-	
-	return render_template("index.html", data=sentence)	
+		# sentence = request.form["sentence"]		
+	if request.method == "POST":
+		sentence = request.form["sentence"]		
+		return render_template("predict.html", data=sentence)	
+	return render_template("predict.html")	
 
 if __name__ == "__main__":
 	app.run(debug=True)
